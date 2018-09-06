cabe3d150d9600036c91171d62131d434a2f8e14
Akshay Borgharkar <akshaycbor@gmail.com>
2018-09-05 21:17:09 -0400

    Update to assign chunks to each worker

--- supervisor.exs
+++ supervisor.exs
@@ -6,33 +6,36 @@ defmodule Parent do
     end
 
     def init([limit|max_length]) do
-        # IO.puts(limit) 
-        children = Enum.map( Enum.to_list(1..limit), fn(x) -> 
-            worker(Child, [[x|max_length]], [id: x, restart: :transient]) 
+        chunked_lists = Enum.chunk_every(Enum.to_list(1..limit), 1000)
+        children = Enum.map( chunked_lists, fn(x) -> 
+            worker(Child, [{x,max_length}], [id: x, restart: :transient]) 
         end )
         
         supervise(children, strategy: :one_for_one)
     end
-
 end
 
 defmodule Child do
     
     def start_link(limits) do
-        # IO.puts(Enum.at(limits, 0))
         pid = spawn_link(__MODULE__, :init, [limits])
         {:ok, pid}
     end
 
-    def init([limit|max_length]) do
-        x = get_sum_of_squares(limit, Enum.at(max_length,0))
-        IO.puts(x)
+    def init({chunked_list,max_length}) do
+        Enum.each chunked_list, fn first_number ->
+            x = get_sum_of_squares(first_number, Enum.at(max_length,0))
+            x = :math.sqrt(x)
+            if x==trunc(x) do 
+                IO.puts(first_number) 
+            end
+        end
     end
 
-    def get_sum_of_squares(limit, 1) do limit*limit end
-    def get_sum_of_squares(limit, max_length) do limit*limit + get_sum_of_squares(limit+1, max_length-1) end
+    def get_sum_of_squares(x, 1) do x*x end
+    def get_sum_of_squares(x, max_length) do x*x + get_sum_of_squares(x+1, max_length-1) end
 end
 
-Parent.start_link([4,4])
+Parent.start_link([1000000,4])
 
 Process.sleep 10_000
